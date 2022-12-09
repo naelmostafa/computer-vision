9f1f6b33b4664196c20f3e83d947c410ef7e3717
ahmad-marouf <ahm.marouf@gmail.com>
2022-12-09 20:51:29 +0200

    Added dynamic programming method

--- Assignment-3/utils.py
+++ Assignment-3/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 SSD = lambda window1, window2: np.sum((window1.astype("float") - window2.astype("float")) ** 2)
 SAD = lambda window1, window2: np.sum(np.abs(window1.astype("float") - window2.astype("float")))
@@ -37,3 +38,60 @@ def block_matching(img1, img2, window_size=3, max_disparity=16, metric=SSD):
             disparity_map[y, x] = best_disparity
     return disparity_map
 
+
+matchCost = lambda pixelL, pixelR, sigma: ((pixelL.astype('float') - pixelR.astype('float'))**2 // sigma**2)
+
+def dp_forwardpass(imgLeft, imgRight, sigma, c0):
+
+    (heightL, widthL), (heightR, widthR) = imgLeft.shape, imgRight.shape
+    height = min(heightL, heightR)
+    
+    # Define the cost matrix
+    cost_matrix = np.zeros((height, widthL, widthR), dtype='float16')
+
+    # Computing Forward Pass
+    for scanline in tqdm(range(height)):
+        # Initializing first row and column of cost matrix
+        for i in range(widthL):
+            cost_matrix[scanline][i][0] = i*c0
+        for i in range(widthR):
+            cost_matrix[scanline][0][i] = i*c0
+
+        # Computing optimal cost
+        for i in range(1, widthL):
+            for j in range(1, widthR):
+
+                costs = [cost_matrix[scanline][i-1][j-1] + matchCost(imgLeft[scanline][i], imgRight[scanline][j], sigma),
+                        cost_matrix[scanline][i-1][j] + c0,
+                        cost_matrix[scanline][i][j-1] + c0]
+
+                cost_matrix[scanline][i][j] = min(costs)
+
+    return cost_matrix
+
+def dp_backpass(cost_matrix):
+
+    height, widthL, widthR = cost_matrix.shape[0], cost_matrix.shape[1], cost_matrix.shape[2]
+
+    # Define the disparity maps
+    disparity_mapL = np.zeros((height, widthL), dtype='float16')
+    disparity_mapR = np.zeros((height, widthR), dtype='float16')
+
+    # Computing Back Pass
+    for scanline in range(height):
+        
+        i = widthL-1; j = widthR-1
+        while (i > 0 and j > 0):
+            _min = np.argmin([cost_matrix[scanline][i-1][j-1], cost_matrix[scanline][i-1][j], cost_matrix[scanline][i][j-1]])
+            if _min == 0: # Pixels i and j match
+                disparity_mapL[scanline][i] = np.abs(j - i)
+                disparity_mapR[scanline][j] = np.abs(j - i)
+                i-=1; j-=1
+            elif _min == 1: # Pixel i is occluded   
+                disparity_mapL[scanline][i] = 0
+                i-=1
+            else: # Pixel j is occluded
+                disparity_mapR[scanline][j] = 0
+                j-=1
+
+    return disparity_mapL, disparity_mapR
